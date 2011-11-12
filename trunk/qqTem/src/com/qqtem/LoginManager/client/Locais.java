0ebd7bc38f42afec467a9b2e8afad1c3f3511f96
eyhiga@gmail.com <eyhiga@gmail.com@1bde1bba-91bf-470f-d1d5-71320a36f175>
2011-11-12 15:56:06 +0000

    Refactoring

--- trunk/qqTem/src/com/qqtem/LoginManager/client/Locais.java
+++ trunk/qqTem/src/com/qqtem/LoginManager/client/Locais.java
@@ -32,13 +32,13 @@ public class Locais extends Composite {
 	TextBox txtEnderecoPopup;
 	TextBox txtNomePopup;
 	ListBox listTipoPopup;
-	
+
 	int contRows;
 
 	public Locais() {
 
 		contRows = 0;
-		
+
 		flexTable = new FlexTable();
 		initWidget(flexTable);
 
@@ -61,9 +61,42 @@ public class Locais extends Composite {
 		flexTable_1.setWidget(0, 1, txtbxTxtnome);
 		txtbxTxtnome.setWidth("300");
 
-		Button btnPesquisar = new Button("Pesquisar");
+		Button btnPesquisar = criarBotaoPesquisar();
 		flexTable_1.setWidget(0, 2, btnPesquisar);
 
+		Button btnAdicionar = criarBotaoAdicionar();
+		flexTable_1.setWidget(0, 3, btnAdicionar);
+
+		Button btnRemover = criarBotaoRemover();
+		flexTable_1.setWidget(0, 4, btnRemover);
+
+		SimplePanel simplePanel_1 = new SimplePanel();
+		flexTable.setWidget(1, 0, simplePanel_1);
+
+		dataTable = new FlexTable();
+		simplePanel_1.setWidget(dataTable);
+		dataTable.setSize("100%", "100%");
+
+		dataTable.setText(0, 0, "Selecionar");
+		dataTable.setText(0, 1, "Nome");
+		dataTable.setText(0, 2, "Endereço");
+		dataTable.setText(0, 3, "Tipo");
+
+		dataTable.setCellPadding(6);
+		dataTable.getRowFormatter().addStyleName(0, "tableListHeader");
+		dataTable.addStyleName("tableList");
+		dataTable.getCellFormatter().addStyleName(0, 0, "tableListSelecionar");
+		dataTable.getCellFormatter().addStyleName(0, 1, "tableListNome");
+		dataTable.getCellFormatter().addStyleName(0, 2, "tableListEndereco");
+		dataTable.getCellFormatter().addStyleName(0, 3, "tableListTipo");
+
+
+	}
+
+	private Button criarBotaoPesquisar()
+	{
+		Button btnPesquisar = new Button("Pesquisar");
+		
 		btnPesquisar.addClickHandler(new ClickHandler()
 		{
 			@Override
@@ -135,145 +168,33 @@ public class Locais extends Composite {
 			}
 		});
 
+		return btnPesquisar;
+		
+	}
 
-
+	private Button criarBotaoAdicionar()
+	{
 		Button btnAdicionar = new Button("Adicionar");
-		flexTable_1.setWidget(0, 3, btnAdicionar);
-
+		
 		btnAdicionar.addClickHandler(new ClickHandler()
 		{
 			@Override
 			public void onClick(ClickEvent event) {
 
-				dialog = new DialogBox();
-				dialog.setTitle("Adicionar local");
-				FlexTable dialogContent = new FlexTable();
-
-				VerticalPanel vertNome = new VerticalPanel();
-				dialogContent.setWidget(0, 0, vertNome);
-
-				FlexTable flexNome = new FlexTable();
-				vertNome.add(flexNome);
-
-				Label lblNome = new Label("Nome: ");
-				lblNome.setWidth(new String("70px"));
-				lblNome.setHorizontalAlignment(Label.ALIGN_RIGHT);
-				flexNome.setWidget(0, 1, lblNome);
-				txtNomePopup = new TextBox();
-				txtNomePopup.setName("txtNome");
-				txtNomePopup.setWidth(new String("200px"));
-				flexNome.setWidget(0, 2, txtNomePopup);
-
-				VerticalPanel vertEndereco = new VerticalPanel();
-				dialogContent.setWidget(1, 0, vertEndereco);
-
-				FlexTable flexEndereco = new FlexTable();
-				vertNome.add(flexEndereco);
-
-				Label lblEndereco = new Label("Endereço: ");
-				lblEndereco.setWidth(new String("70px"));
-				lblEndereco.setHorizontalAlignment(Label.ALIGN_RIGHT);
-				flexEndereco.setWidget(0, 1, lblEndereco);
-				txtEnderecoPopup = new TextBox();
-				txtEnderecoPopup.setName("txtNome");
-				txtEnderecoPopup.setWidth(new String("200px"));
-				flexEndereco.setWidget(0, 2, txtEnderecoPopup);
-
-				VerticalPanel vertTipo = new VerticalPanel();
-				dialogContent.setWidget(2, 0, vertTipo);
-
-				FlexTable flexTipo = new FlexTable();
-				vertTipo.add(flexTipo);
-
-				Label lblTipo = new Label("Tipo: ");
-				lblTipo.setWidth(new String("70px"));
-				lblTipo.setHorizontalAlignment(Label.ALIGN_RIGHT);
-				flexTipo.setWidget(0, 1, lblTipo);
-				listTipoPopup = new ListBox();
-				listTipoPopup.setWidth(new String("200px"));
-				listTipoPopup.setName("listTipoPopup");
-				listTipoPopup.addItem("Restaurante");
-				listTipoPopup.addItem("Hotel");
-				listTipoPopup.addItem("Bar");
-				listTipoPopup.addItem("Parque");
-				flexTipo.setWidget(0, 2, listTipoPopup);
-
-				VerticalPanel vertDesc = new VerticalPanel();
-				dialogContent.setWidget(3, 0, vertDesc);
-
-				FlexTable flexDesc = new FlexTable();
-				vertDesc.add(flexDesc);
-				Label lblDesc = new Label("Descrição: ");
-				lblDesc.setWidth(new String("70px"));
-				lblDesc.setHorizontalAlignment(Label.ALIGN_RIGHT);
-				flexDesc.setWidget(0, 0, lblDesc);
-				TextArea txtDesc = new TextArea();
-				txtDesc.setWidth(new String("270px"));
-				flexDesc.setWidget(1, 0, txtDesc);
-
-				VerticalPanel vertBotoes = new VerticalPanel();
-				dialogContent.setWidget(4, 0, vertBotoes);
-
-				FlexTable flexBotoes = new FlexTable();
-				vertBotoes.add(flexBotoes);
-				Button btnAddFotos = new Button("Adicionar fotos");
-				Button btnSalvar = new Button("Salvar");
-
-
-				btnSalvar.addClickHandler(new ClickHandler()
-				{
-					@Override
-					public void onClick(ClickEvent event)
-					{
-						int row = dataTable.getRowCount();
-
-						contRows++;
-						
-						String nome = txtNomePopup.getText();
-						String end = txtEnderecoPopup.getText();
-						String tipo = listTipoPopup.getItemText(listTipoPopup.getSelectedIndex());
-
-						CheckBox chk = new CheckBox();
-								
-						
-						dataTable.setWidget(row, 0, chk);
-						
-						Label n = new Label(nome);
-						Label e = new Label(end);
-						Label t = new Label(tipo);
-						
-						dataTable.setWidget(row, 1, n);
-						dataTable.setWidget(row, 2, e);
-						dataTable.setWidget(row, 3, t);
-						dialog.hide();
-					}
-				});
-
-				Button btnCancelar = new Button("Cancelar");
-
-				btnCancelar.addClickHandler(new ClickHandler()
-				{
-					@Override
-					public void onClick(ClickEvent event)
-					{
-						dialog.hide();
-					}
-				});
-
-				flexBotoes.setWidget(0, 0, btnAddFotos);
-				flexBotoes.setWidget(0, 5, btnSalvar);
-				flexBotoes.setWidget(0, 6, btnCancelar);
-
-				dialog.setWidget(dialogContent);
-
+				dialog = criarDialogAdicionar();
+				
 				dialog.center();
 				dialog.show();
 			}
 		});
-
-		Button btnRemover = new Button("Remover selecionados");
-		flexTable_1.setWidget(0, 4, btnRemover);
-
+		
+		return btnAdicionar;
+	}
+	
+	private Button criarBotaoRemover()
+	{
+		Button btnRemover = new Button("Remover");
+		
 		btnRemover.addClickHandler(new ClickHandler()
 		{
 			@Override
@@ -297,29 +218,136 @@ public class Locais extends Composite {
 
 			}
 		});
-
-		SimplePanel simplePanel_1 = new SimplePanel();
-		flexTable.setWidget(1, 0, simplePanel_1);
-
-		dataTable = new FlexTable();
-		dataTable.setStyleName((String) null);
-		simplePanel_1.setWidget(dataTable);
-		dataTable.setSize("100%", "100%");
-
-		dataTable.setText(0, 0, "Selecionar");
-		dataTable.setText(0, 1, "Nome");
-		dataTable.setText(0, 2, "Endereço");
-		dataTable.setText(0, 3, "Tipo");
-
-		dataTable.setCellPadding(6);
-		dataTable.getRowFormatter().addStyleName(0, "tableListHeader");
-		dataTable.addStyleName("watchList");
-		dataTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
-		dataTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
-		dataTable.getCellFormatter().addStyleName(0, 3, "watchListNumericColumn");
-
+		
+		return btnRemover;
+		
 	}
 
+	private DialogBox criarDialogAdicionar()
+	{
+		DialogBox diag = new DialogBox();
+		
+		diag.setTitle("Adicionar local");
+		FlexTable dialogContent = new FlexTable();
 
+		VerticalPanel vertNome = new VerticalPanel();
+		dialogContent.setWidget(0, 0, vertNome);
+
+		FlexTable flexNome = new FlexTable();
+		vertNome.add(flexNome);
+
+		Label lblNome = new Label("Nome: ");
+		lblNome.setWidth(new String("70px"));
+		lblNome.setHorizontalAlignment(Label.ALIGN_RIGHT);
+		flexNome.setWidget(0, 1, lblNome);
+		txtNomePopup = new TextBox();
+		txtNomePopup.setName("txtNome");
+		txtNomePopup.setWidth(new String("200px"));
+		flexNome.setWidget(0, 2, txtNomePopup);
+
+		VerticalPanel vertEndereco = new VerticalPanel();
+		dialogContent.setWidget(1, 0, vertEndereco);
+
+		FlexTable flexEndereco = new FlexTable();
+		vertNome.add(flexEndereco);
+
+		Label lblEndereco = new Label("Endereço: ");
+		lblEndereco.setWidth(new String("70px"));
+		lblEndereco.setHorizontalAlignment(Label.ALIGN_RIGHT);
+		flexEndereco.setWidget(0, 1, lblEndereco);
+		txtEnderecoPopup = new TextBox();
+		txtEnderecoPopup.setName("txtNome");
+		txtEnderecoPopup.setWidth(new String("200px"));
+		flexEndereco.setWidget(0, 2, txtEnderecoPopup);
+
+		VerticalPanel vertTipo = new VerticalPanel();
+		dialogContent.setWidget(2, 0, vertTipo);
+
+		FlexTable flexTipo = new FlexTable();
+		vertTipo.add(flexTipo);
+
+		Label lblTipo = new Label("Tipo: ");
+		lblTipo.setWidth(new String("70px"));
+		lblTipo.setHorizontalAlignment(Label.ALIGN_RIGHT);
+		flexTipo.setWidget(0, 1, lblTipo);
+		listTipoPopup = new ListBox();
+		listTipoPopup.setWidth(new String("200px"));
+		listTipoPopup.setName("listTipoPopup");
+		listTipoPopup.addItem("Restaurante");
+		listTipoPopup.addItem("Hotel");
+		listTipoPopup.addItem("Bar");
+		listTipoPopup.addItem("Parque");
+		flexTipo.setWidget(0, 2, listTipoPopup);
+
+		VerticalPanel vertDesc = new VerticalPanel();
+		dialogContent.setWidget(3, 0, vertDesc);
+
+		FlexTable flexDesc = new FlexTable();
+		vertDesc.add(flexDesc);
+		Label lblDesc = new Label("Descrição: ");
+		lblDesc.setWidth(new String("70px"));
+		lblDesc.setHorizontalAlignment(Label.ALIGN_RIGHT);
+		flexDesc.setWidget(0, 0, lblDesc);
+		TextArea txtDesc = new TextArea();
+		txtDesc.setWidth(new String("270px"));
+		flexDesc.setWidget(1, 0, txtDesc);
+
+		VerticalPanel vertBotoes = new VerticalPanel();
+		dialogContent.setWidget(4, 0, vertBotoes);
+
+		FlexTable flexBotoes = new FlexTable();
+		vertBotoes.add(flexBotoes);
+		Button btnAddFotos = new Button("Adicionar fotos");
+		Button btnSalvar = new Button("Salvar");
+
+		btnSalvar.addClickHandler(new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent event)
+			{
+				int row = dataTable.getRowCount();
+
+				contRows++;
+
+				String nome = txtNomePopup.getText();
+				String end = txtEnderecoPopup.getText();
+				String tipo = listTipoPopup.getItemText(listTipoPopup.getSelectedIndex());
+
+				CheckBox chk = new CheckBox();
+
+				dataTable.setWidget(row, 0, chk);
+
+				Label n = new Label(nome);
+				Label e = new Label(end);
+				Label t = new Label(tipo);
+
+				dataTable.setWidget(row, 1, n);
+				dataTable.setWidget(row, 2, e);
+				dataTable.setWidget(row, 3, t);
+				dialog.hide();
+			}
+		});
+
+		Button btnCancelar = new Button("Cancelar");
+
+		btnCancelar.addClickHandler(new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent event)
+			{
+				dialog.hide();
+			}
+		});
+
+		flexBotoes.setWidget(0, 0, btnAddFotos);
+		flexBotoes.setWidget(0, 5, btnSalvar);
+		flexBotoes.setWidget(0, 6, btnCancelar);
+
+		diag.setWidget(dialogContent);
+
+		return diag;
+		
+	}
+	
 }
 
