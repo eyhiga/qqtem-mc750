33e733490043887cb1056de2d417de750f0b4b27
nando0205@gmail.com <nando0205@gmail.com@1bde1bba-91bf-470f-d1d5-71320a36f175>
2011-11-23 00:54:19 +0000

    Usuarios, com alguns bugs, mas ja vou tentar corrigir

--- qqTem/src/com/qqtem/LoginManager/client/UsuarioDialog.java
+++ qqTem/src/com/qqtem/LoginManager/client/UsuarioDialog.java
@@ -1,5 +1,8 @@
 package com.qqtem.LoginManager.client;
 
+import javax.swing.JOptionPane;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Popup;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Label;
@@ -11,10 +14,28 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.impl.PopupImpl;
+import com.google.gwt.dev.util.msg.Message;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 
 public class UsuarioDialog extends DialogBox {
 
-	public UsuarioDialog() {
+	private Usuarios usuarios;
+	private TextBox txtbxLogin;
+	private TextBox txtbxNome;
+	private RadioButton rdbtnMasculino;
+	private RadioButton rdbtnFeminino;
+	private RadioButton rbState[] = new RadioButton[3];
+	private UserData editingUser;
+	private boolean saved;
+	private RadioButton rdbtnAdmin;
+	private RadioButton rdbtnNormal;
+	private RadioButton rdbtnBanido;
+	
+	public UsuarioDialog(Usuarios usr) {
+		this.usuarios = usr;
+		saved = false;
 		setText("Usuário");
 		setHTML("<center><h1>Usuário</h1></center>");
 		
@@ -25,14 +46,14 @@ public class UsuarioDialog extends DialogBox {
 		Label lblLogin = new Label("Login");
 		flexTable.setWidget(0, 0, lblLogin);
 		
-		TextBox txtbxLogin = new TextBox();
+		txtbxLogin = new TextBox();
 		txtbxLogin.setText("Login");
 		flexTable.setWidget(0, 1, txtbxLogin);
 		
 		Label lblNome = new Label("Nome");
 		flexTable.setWidget(1, 0, lblNome);
 		
-		TextBox txtbxNome = new TextBox();
+		txtbxNome = new TextBox();
 		txtbxNome.setText("Nome");
 		flexTable.setWidget(1, 1, txtbxNome);
 		
@@ -43,20 +64,87 @@ public class UsuarioDialog extends DialogBox {
 		horizontalPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 		flexTable.setWidget(2, 1, horizontalPanel);
 		
-		RadioButton rdbtnMasculino = new RadioButton("new name", "Masculino");
+		rdbtnMasculino = new RadioButton("gender", "Masculino");
 		horizontalPanel.add(rdbtnMasculino);
 		
-		RadioButton rdbtnFeminino = new RadioButton("new name", "Feminino");
+		rdbtnFeminino = new RadioButton("gender", "Feminino");
 		horizontalPanel.add(rdbtnFeminino);
+		
+		Label lblEstado = new Label("Estado");
+		flexTable.setWidget(3, 0, lblEstado);
+		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		flexTable.setWidget(3, 1, verticalPanel);
+		
+		rdbtnAdmin = new RadioButton("state", "Admin");
+		rdbtnAdmin.setHTML("Administrador");
+		verticalPanel.add(rdbtnAdmin);
+		
+		
+		rdbtnNormal = new RadioButton("state", "Normal");
+		verticalPanel.add(rdbtnNormal);
+		
+		rdbtnBanido = new RadioButton("state", "Banido");
+		verticalPanel.add(rdbtnBanido);
 		
 		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
 		horizontalPanel_1.setSpacing(5);
-		flexTable.setWidget(3, 1, horizontalPanel_1);
+		flexTable.setWidget(4, 1, horizontalPanel_1);
 		
 		Button btnSalvar = new Button("Salvar");
+		btnSalvar.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				editingUser.setLogin(txtbxLogin.getText());
+				editingUser.setName(txtbxNome.getText());
+				editingUser.setMale(rdbtnMasculino.getValue());
+				if(rdbtnAdmin.getValue())
+					editingUser.setState(UserData.ADMIN);
+				else if(rdbtnBanido.getValue())
+					editingUser.setState(UserData.BANNED);
+				else
+					editingUser.setState(UserData.NORMAL);
+				
+				usuarios.editUser(editingUser);
+				hide();
+			}
+		});
 		horizontalPanel_1.add(btnSalvar);
 		
 		Button btnCancelar = new Button("Cancelar");
+		btnCancelar.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				UsuarioDialog.this.hide();
+			}
+		});
 		horizontalPanel_1.add(btnCancelar);
+		
+		rbState[0] = rdbtnNormal;
+		rbState[1] = rdbtnAdmin;
+		rbState[2] = rdbtnBanido;
 	}
+	
+	void showUser(UserData user){
+		saved = false;
+		editingUser = user;
+		
+		txtbxLogin.setText(user.getLogin());
+		txtbxNome.setText(user.getName());
+		if(user.isMale())
+			rdbtnMasculino.setValue(true);
+		else
+			rdbtnFeminino.setValue(true);
+		
+		rbState[user.getState()].setValue(true);
+	}
+
+	public UserData getEditingUser(){
+		return editingUser;
+	}
+	
+	public boolean wasSaved(){
+		return saved;
+	}
+	
+
+	
 }
--- qqTem/src/com/qqtem/LoginManager/client/Usuarios.java
+++ qqTem/src/com/qqtem/LoginManager/client/Usuarios.java
@@ -1,5 +1,7 @@
 package com.qqtem.LoginManager.client;
 
+import java.util.Vector;
+
 import com.google.gwt.cell.client.Cell;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
@@ -23,11 +25,19 @@ import com.google.gwt.view.client.NoSelectionModel;
 import com.google.gwt.view.client.TreeViewModel.NodeInfo;
 import com.google.gwt.view.client.TreeViewModel.DefaultNodeInfo;
 import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.ListBox;
+import com.ibm.icu.impl.LocaleDisplayNamesImpl.DataTable;
 
 public class Usuarios extends Composite {
-
+	private Vector<UserData> usuarios;
+	private FlexTable dataTable;
+	private UsuarioDialog dialog;
+	
 	public Usuarios() {
+		dialog = new UsuarioDialog(this);
+		usuarios = new Vector<UserData>();
 		
 		FlexTable flexTable = new FlexTable();
 		initWidget(flexTable);
@@ -48,18 +58,84 @@ public class Usuarios extends Composite {
 		horizontalPanel.add(btnPesquisar);
 		flexTable.getCellFormatter().setVerticalAlignment(0, 0, HasVerticalAlignment.ALIGN_TOP);
 		
-		FlexTable dataTable = new FlexTable();
+		dataTable = new FlexTable();
 		flexTable.setWidget(1, 0, dataTable);
 		dataTable.setSize("100%", "100%");
 		
-		dataTable.setText(0, 0, "Selecionar");
+		dataTable.setText(0, 0, "Nome");
 		dataTable.setText(0, 1, "Login");
-		dataTable.setText(0, 2, "Nome");
+		dataTable.setText(0, 2, "Sexo");
 		dataTable.setText(0, 3, "Estado");
 		
-		for (int i = 0; i < dataTable.getCellCount(0); i++) {
-			comboBox.addItem(dataTable.getText(0, i));
-		}
-	}
+		dataTable.getCellFormatter().addStyleName(0,0,"tableListHeader");
+		dataTable.getCellFormatter().addStyleName(0,1,"tableListHeader");
+		dataTable.getCellFormatter().addStyleName(0,2,"tableListHeader");
+		dataTable.getCellFormatter().addStyleName(0,3,"tableListHeader");
+		
+		adicionaUsuario("Joao", "jojo22", true , UserData.NORMAL);
+		adicionaUsuario("gangster", "gang1337", true ,UserData.BANNED);
+		adicionaUsuario("Maria Joana", "marylu", false ,UserData.NORMAL);
+		adicionaUsuario("Administrador","admSkyrim",true ,UserData.ADMIN);
+		
+		
+		
+		dataTable.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				int index = dataTable.getCellForEvent(event).getRowIndex();
+				if(index>0){
+					openUserDialog(index-1);
+				}
+			}
 
+		});
+		
+	}
+	
+	private void openUserDialog(int index){
+		System.out.println(usuarios.elementAt(index).getId());
+		dialog.showUser(usuarios.elementAt(index));
+		dialog.center();
+	}
+	public void adicionaUsuario(String nome, String login,boolean male,int estado){
+		usuarios.add(new UserData(usuarios.size(), nome, login,male, estado));
+		
+		System.out.println(usuarios.lastElement().getId());
+		
+		dataTable.setText(usuarios.size(), 0, nome);
+		dataTable.setText(usuarios.size(), 1, login);
+		dataTable.setText(usuarios.size(), 2, male? "Masculino":"Feminino");
+		dataTable.setText(usuarios.size(), 3, getUserStateString(estado));
+		dataTable.getRowFormatter().addStyleName(
+				usuarios.size(), usuarios.size()%2==0? "tableList-odd":"tableList-even");
+	}
+	
+	public String getUserStateString(int state){
+		switch(state){
+		case UserData.ADMIN:
+			return "Administrador";
+		case UserData.BANNED:
+			return "Banido";
+		case UserData.NORMAL:
+			return "Normal";
+		default:
+			return "Desconhecido";
+		}
+
+	}
+	
+	
+	public void editUser(UserData editingUser) {
+		int id = editingUser.getId();
+		
+//		System.out.println(id);
+		dataTable.setText(id+1, 0, editingUser.getName());
+		dataTable.setText(id+1, 1, editingUser.getLogin());
+		dataTable.setText(id+1, 2, editingUser.isMale()? "Masculino":"Feminino");
+		dataTable.setText(id+1, 3, getUserStateString(editingUser.getState()));
+		
+	}
+	
+	
 }
